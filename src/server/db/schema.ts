2e6b5f75874171a513ccaca8cab3b277ced47e92
Facundo Martin <facundomartin98@gmail.com>
2025-09-03 17:17:16 -0300

    refactor(RAG): create embeddings db schema, consolidate timestamps

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,4 +1,5 @@
 import {
+  index,
   integer,
   pgEnum,
   pgTable,
@@ -6,27 +7,53 @@ import {
   text,
   timestamp,
   varchar,
+  vector,
 } from "drizzle-orm/pg-core";
 
 export const userSystemEnum = pgEnum("user_system_enum", ["assistant", "user"]);
+
+const timestamps = {
+  updated_at: timestamp(),
+  created_at: timestamp().defaultNow().notNull(),
+  deleted_at: timestamp(),
+};
 
 export const chats = pgTable("chats", {
   id: serial("id").primaryKey(),
   pdfName: text("pdf_name").notNull(),
   pdfUrl: text("pdf_url").notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  userId: varchar("user_id", { length: 258 }).notNull(),
+  content: text("content").notNull(),
   fileKey: text("file_key").notNull(),
+  userId: varchar("user_id", { length: 258 }).notNull(),
+  ...timestamps,
 });
 
 export const messages = pgTable("messages", {
   id: serial("id").primaryKey(),
   chatId: integer("chat_id")
-    .references(() => chats.id)
+    .references(() => chats.id, { onDelete: "cascade" })
     .notNull(),
   body: text("body").notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
   role: userSystemEnum("role").notNull(),
+  ...timestamps,
 });
+
+export const embeddings = pgTable(
+  "embeddings",
+  {
+    id: serial("id").primaryKey(),
+    chatId: integer("chat_id").references(() => chats.id, {
+      onDelete: "cascade",
+    }),
+    content: text("content").notNull(),
+    embedding: vector("embedding", { dimensions: 1536 }).notNull(),
+  },
+  (table) => [
+    index("embeddingIndex").using(
+      "hnsw",
+      table.embedding.op("vector_cosine_ops"),
+    ),
+  ],
+);
 
 export type SelectChat = typeof chats.$inferSelect;
